439d077baa876adf8140aba3f15aebc2d89151f4
arthur <ajcvanittersum@gmail.com>
2025-02-28 14:42:03 +0100

    Drawing multiple rooms
    Added code to draw multiple rooms,storing them in a list and drawing all rooms from the list

--- warm-up-assignment_student/Assets/Scripts/DungeonGenerator.cs
+++ warm-up-assignment_student/Assets/Scripts/DungeonGenerator.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 public class DungeonGenerator : MonoBehaviour
 {
     public RectInt test1 = new RectInt(0, 0, 100, 50);
-    
-    
+    public RectInt test2 = new RectInt(0, 50, 100, 50);
+    private List<RectInt> rooms = new List<RectInt>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rooms.Add(test1);
+        rooms.Add(test2);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        AlgorithmsUtils.DebugRectInt(test1, Color.red);
+        for (int i = 0; i < rooms.Count; i++)
+        {
+            AlgorithmsUtils.DebugRectInt(rooms[i], Color.red);
+        }
+        
     }
 }
