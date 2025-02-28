54f05b8465e26f6543d01da80aad9f7f96ec5201
arthur <ajcvanittersum@gmail.com>
2025-02-28 15:21:14 +0100

    Added splitroom function
    Added splitroom function room1 is split and displayed. split function only splits horizontally.

--- warm-up-assignment_student/Assets/Scripts/DungeonGenerator.cs
+++ warm-up-assignment_student/Assets/Scripts/DungeonGenerator.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 
 public class DungeonGenerator : MonoBehaviour
 {
-    public RectInt test1 = new RectInt(0, 0, 100, 50);
-    public RectInt test2 = new RectInt(0, 50, 100, 50);
-    private List<RectInt> rooms = new List<RectInt>();
+    public RectInt startSquare = new RectInt(10, 0, 100, 100);
+    
+    public List<RectInt> rooms = new List<RectInt>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rooms.Add(test1);
-        rooms.Add(test2);
+        rooms.Add(startSquare);
+        SplitRoom(rooms[0]);
     }
 
     // Update is called once per frame
@@ -22,5 +22,17 @@ public class DungeonGenerator : MonoBehaviour
             AlgorithmsUtils.DebugRectInt(rooms[i], Color.red);
         }
         
+    }
+
+    void SplitRoom(RectInt roomToSplit)
+    {
+        //first only horizontally
+        int splitDistance = roomToSplit.width / 2;
+        Vector2Int newSize = new Vector2Int(splitDistance, roomToSplit.size.y);
+        Vector2Int newPos  = new Vector2Int(splitDistance + roomToSplit.position.x, roomToSplit.position.y);
+        RectInt split1 = new RectInt(roomToSplit.position, newSize);
+        RectInt split2 = new RectInt(newPos, newSize);
+        rooms.Add(split1);
+        rooms.Add(split2);
     }
 }
