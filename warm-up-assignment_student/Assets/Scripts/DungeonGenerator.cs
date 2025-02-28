922d0ebcf23da674eafbd3f14b2578daf95d921a
arthur <ajcvanittersum@gmail.com>
2025-02-28 16:52:51 +0100

    Vertical room splitting, debug, split random
    I created a function for splitting vertically. I also added some debug stuff, like drawing in random colors. i also made the rooms split at a random place in the rectangle.

--- warm-up-assignment_student/Assets/Scripts/DungeonGenerator.cs
+++ warm-up-assignment_student/Assets/Scripts/DungeonGenerator.cs
@@ -6,12 +6,21 @@ public class DungeonGenerator : MonoBehaviour
     public RectInt startSquare = new RectInt(10, 0, 100, 100);
     
     public List<RectInt> rooms = new List<RectInt>();
+    public List<Color> colors = new List<Color>();
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rooms.Add(startSquare);
-        SplitRoom(rooms[0]);
+        SplitRoomHorizontal(rooms[0]);
+        SplitRoomVertical(rooms[1]);
+        SplitRoomVertical(rooms[2]);
+
+        for (int i = 0; i < rooms.Count; i++)
+        {
+            colors.Add(Random.ColorHSV(0f, 1f, 0.5f, 1f, 0.5f, 1f));
+        }
     }
 
     // Update is called once per frame
@@ -19,19 +28,47 @@ public class DungeonGenerator : MonoBehaviour
     {
         for (int i = 0; i < rooms.Count; i++)
         {
-            AlgorithmsUtils.DebugRectInt(rooms[i], Color.red);
+            AlgorithmsUtils.DebugRectInt(rooms[i], colors[i],0,false,i*10);
+            //RectInt smaller = new RectInt(rooms[i].position.x + 1, rooms[i].position.y + 1, rooms[i].size.x - 2, rooms[i].size.y - 2);
+
+
+            //AlgorithmsUtils.DebugRectInt(smaller, colors[i],0,false,i * 10);
         }
         
     }
-
-    void SplitRoom(RectInt roomToSplit)
+    
+    void SplitRoomHorizontal(RectInt roomToSplit)
     {
-        //first only horizontally
-        int splitDistance = roomToSplit.width / 2;
-        Vector2Int newSize = new Vector2Int(splitDistance, roomToSplit.size.y);
+
+        if (roomToSplit.width < 10)
+        {
+            return;
+        }
+        int splitDistance = Random.Range(3, roomToSplit.width - 3);
+
+
+        Vector2Int newSize1 = new Vector2Int(splitDistance, roomToSplit.size.y);
+        Vector2Int newSize2 = new Vector2Int(roomToSplit.size.x - splitDistance, roomToSplit.size.y);
         Vector2Int newPos  = new Vector2Int(splitDistance + roomToSplit.position.x, roomToSplit.position.y);
-        RectInt split1 = new RectInt(roomToSplit.position, newSize);
-        RectInt split2 = new RectInt(newPos, newSize);
+        RectInt split1 = new RectInt(roomToSplit.position, newSize1);
+        RectInt split2 = new RectInt(newPos, newSize2);
+        rooms.Add(split1);
+        rooms.Add(split2);
+    }
+    void SplitRoomVertical(RectInt roomToSplit)
+    {
+        if (roomToSplit.height < 10)
+        {
+            return;
+        }
+        int splitDistance = Random.Range(3, roomToSplit.height - 3);
+
+
+        Vector2Int newSize1 = new Vector2Int(roomToSplit.size.x, splitDistance);
+        Vector2Int newSize2 = new Vector2Int(roomToSplit.size.x, roomToSplit.size.y - splitDistance);
+        Vector2Int newPos = new Vector2Int(roomToSplit.position.x, splitDistance + roomToSplit.position.y);
+        RectInt split1 = new RectInt(roomToSplit.position, newSize1);
+        RectInt split2 = new RectInt(newPos, newSize2);
         rooms.Add(split1);
         rooms.Add(split2);
     }
